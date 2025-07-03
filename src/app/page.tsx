5240e85d106f3a9e7aca86c3c0a1a865485c9e91
Kevin <106498220+Kga-6@users.noreply.github.com>
2025-07-02 20:03:54 -0400

    Ready 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home(){
     return (
       <>
         <h1>Welcome to Breadboard</h1>
-        <Link href="/login">Logins</Link>
+        <Link href="/login">Login</Link>
       </>
     )
 }
